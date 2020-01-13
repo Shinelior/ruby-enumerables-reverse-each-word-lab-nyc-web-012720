abc34a8d858789efde5831456abc9ba676132c39
lior Shine <shinelior@gmail.com>
2020-01-13 03:13:50 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,59 @@
+require "pry"
+
+#def reverse_each_word(x=%w(Hello there, and how are you?))
+
+ # z=[]
+# z=%w(Hello there, and how are you?).each_with_object([]){|x,y| y << x.reverse}
+# return z.join(" ")
+# end
+
+#reverse_each_word(%w(Hi again, just making sure its reversed!))
+#def reverse_each_word(x)
+ # z=[]
+# z = x.each_with_object([]){|x,y| y << x.reverse}
+# return z.join(" ")
+# end
+
+ def reverse_each_word(s)
+   i = 0 
+   y = []
+  s = s.split(/\s+/)
+   
+  #s = s.split(",")
+   
+   s.each do |x|  
+     
+    
+       
+        y << s[i].reverse
+      
+  i += 1
+    end
+   y=y.join(" ")
+ end
+ 
+ 
+ 
+ # def collect(s)
+    
+   # i=0
+     #n=[]
+     #s=s.join(" ")
+      y = y.split(/\s+/)
+     
+     y.collect do |x| 
+      
+   y.reverse
+     
+    end
+   # return n
+  end
+    
+     
+     
+   
+    
+#  map { reverse; } split /\s+/ } 
+ 
+    
+   
